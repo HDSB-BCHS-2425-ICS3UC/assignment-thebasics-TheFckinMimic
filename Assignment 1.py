f075da7c7efe3d7f3866cc9e6899b938494f8495
TheFckinMimic <1scrymgeolou@hdsb.ca>
2025-02-26 14:19:21 -0500

    Update Assignment 1.py

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,6 +1,13 @@
 #Louis Scrymgeour
 #2/25/25
 #Description:
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#Shown below is an example of all the functions
+
+#This is a double function
+double = 2.2
+print("Double double on the wall is:", double)
 
 
 #This a float function
@@ -8,11 +15,51 @@ float = 6.66
 print("Floaty floaty on the wall is:", float)
 
 
-#This is an integer
-integer = 21
-print("Integer integer on the wall is:", integer)
+#This is an integer function
+int = 21
+print("Integer integer on the wall is:", int)
 
 
 #This a boolean function
 boolean = True
 print("Boolean boolean on the wall is:", boolean)
+
+
+#This is a char function
+char = 0
+print("Character character on the wall is:", char)
+
+
+#This is a string function
+str = "2, 4, 6, 8"
+print("Stringy stringy on the wall is:", str)
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#Shown below are examples of math operations
+
+import math
+
+#This a square function
+square = math.sqrt(25)
+print("If I was a square I would be:", square)
+
+
+#This is addition
+sum_add = 2+23
+print("If I was a sum_add I would be:", sum_add)
+
+
+#This is subtraction
+difference = 2-23
+print("If I was a differnce I would be:", difference)
+
+
+#This is multiplication
+product = 2*3*4
+
+#This is 2 to the power of 4
+quotient = 31/8
+exponent = 2**4
+rem_1 = 24%6
+rem_2 = 24%5
+rem_3 = 24%7
